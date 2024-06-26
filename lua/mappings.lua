ee0b2735783d45a5ff18b98cb77af7a44fa579c9
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-06-26 16:00:19 +0530

    terminal resize

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,9 +8,6 @@ local map = vim.keymap.set
 map("n", "<Tab>", ":bnext<CR>")
 map("n", "<S-Tab>", ":bprevious<CR>")
 
--- Toggle NvimTree
-map("n", "\\", ":NvimTreeToggle<CR>")
-
 -- Telescope buffers
 map("n", "<leader><leader>", ":Telescope buffers<CR>", { noremap = true, silent = true })
 
@@ -31,14 +28,7 @@ map("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 -- Open terminal in vertical split occupying half the screen
 map("n", "<leader>v", ":VTerm<CR>", { noremap = true, silent = true })
 
--- Open terminal in horizontal split occupying half the screen
-map("n", "<leader>h", ":HTerm<CR>", { noremap = true, silent = true })
-
 -- Custom commands to open terminal splits
 vim.cmd [[
   command! VTerm :vsp | :wincmd l | :resize 50% | :terminal
-]]
-
-vim.cmd [[
-  command! HTerm :sp | :wincmd j | :resize 50% | :terminal
 ]]
