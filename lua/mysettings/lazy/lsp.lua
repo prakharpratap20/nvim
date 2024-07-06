875cbaf02840ee55cfb31aab7ee47603be2e1075
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-07-07 04:33:04 +0530

    fixed lsp and better startuptime

--- lua/mysettings/lazy/lsp.lua
+++ lua/mysettings/lazy/lsp.lua
@@ -1,128 +1,26 @@
 return {
-    "neovim/nvim-lspconfig",
+    "VonHeikemen/lsp-zero.nvim",
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+        "folke/tokyonight.nvim",
+        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
     },
-
     config = function()
         local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
+        local lsp_zero = require('lsp-zero')
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "pylsp",
-                "html",
-                "cssls",
-                "gopls",
-                "jsonls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
+        lsp_zero.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp_zero.default_keymaps({ buffer = bufnr })
+        end)
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                },
-                            },
-                        },
-                    })
-                end,
-                ["pylsp"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.pylsp.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            pylsp = {
-                                plugins = {
-                                    pycodestyle = { enabled = true },
-                                    autopep8 = { enabled = true },
-                                    mccabe = { enabled = false }
-                                },
-                                configurationSources = { "flake8" },
-                                logLevel = "warn",
-                            },
-                            python = {
-                                analysis = {
-                                    typeCheckingMode = "advanced",
-                                    autosearchpaths = true,
-                                    uselibrarycodefortypes = true,
-                                },
-                            },
-                        },
-                    })
-                end,
-                ["html"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.html.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            html = {
-                                analysis = {
-                                    typecheckingmode = "normal",
-                                    autosearchpaths = true,
-                                    uselibrarycodefortypes = true,
-                                },
-                            },
-                        }
-                    })
-                end,
-                ["cssls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.cssls.setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["jsonls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.jsonls.setup({
-                        capabilities = capabilities,
-                    })
-                end,
-            },
-        })
+        require("lspconfig").pyright.setup({})
+        require("lspconfig").emmet_ls.setup({})
+        require("lspconfig").lua_ls.setup({})
+
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
@@ -186,5 +84,5 @@ return {
                 prefix = "",
             },
         })
-    end,
+    end
 }
--- lua/mysettings/set.lua
+++ lua/mysettings/set.lua
@@ -16,7 +16,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true
+-- vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -27,6 +27,8 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.opt.clipboard = "unnamedplus"
 
 -- vim.opt.colorcolumn = "80"
 
@@ -44,5 +46,3 @@ vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "BufEnter" }, {
         end
     end,
 })
-
-vim.opt.clipboard = "unnamedplus"
